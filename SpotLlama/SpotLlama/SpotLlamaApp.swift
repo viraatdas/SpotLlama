4149ae4c467f6e887fcfc2f2c80b7d985270edf2
Viraat <viraat.laldas@gmail.com>
2024-03-24 01:45:02 -0400

    commiting xcode

---
+++ SpotLlama/SpotLlama/SpotLlamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotLlamaApp.swift
+//  SpotLlama
+//
+//  Created by Viraat Das on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotLlamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
