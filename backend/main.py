012cfe0ea89dd04881133ea5536bd9646f099c3d
Viraat Das <viraat.laldas@gmail.com>
2024-06-21 15:45:59 -0700

    it works

--- backend/main.py
+++ backend/main.py
@@ -2,62 +2,50 @@ from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_community.chat_models import ChatOllama
-from langchain.prompts import load_prompt
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
-from langchain_community.embeddings import LlamaCppEmbeddings
-
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+import bs4
+from langchain import hub
 
 DIR_PATH = "test"
+LLAMA_MODEL_PATH = "/path/to/llama_model.bin"  # Specify the path to your Llama model file
+
 llm = ChatOllama(model="llama3")
 
 def load_documents_from_directory(directory):
-    # Initialize the loader with the directory path
     loader = DirectoryLoader(directory)
-    
-    # Load the documents
     documents = loader.load()
-
-    # Print out details of each document
     for doc in documents:
         print(f"Source: {doc.metadata['source']}")
-        print(f"Content: {doc.page_content[:500]}")  # Print the first 500 characters for a quick summary
+        print(f"Content: {doc.page_content[:500]}")
         print("-" * 40)
-    
     return documents
 
 docs = load_documents_from_directory(DIR_PATH)
 
-# Split the documents
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 splits = text_splitter.split_documents(docs)
 
-# Create embeddings and vector store
-vectorstore = Chroma.from_documents(documents=splits, embedding=LlamaCppEmbeddings())
+vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
 
-# Setup retriever
 retriever = vectorstore.as_retriever()
+prompt = hub.pull("rlm/rag-prompt")
 
-# Load the prompt
-prompt = load_prompt("rlm/rag-prompt")
-
-# Define the RAG chain
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
+def invoke_rag_chain(input):
+    # Using LangChain Expressive Language chain syntax
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
 
+    return rag_chain.invoke(input)
 
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm  
-    | StrOutputParser()
-)
-
-
-
-prompt = "Tell me about Venkat?"
-response = rag_chain.invoke(prompt)
-print(response)
-
-
+input = "Tell me about Venkat"
+print(invoke_rag_chain(input))
