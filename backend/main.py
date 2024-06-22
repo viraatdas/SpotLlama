ada53d8a80feefd1d5fafa18f785e1aeb91a1f3b
Viraat Das <viraat.laldas@gmail.com>
2024-06-21 18:06:35 -0700

    backend seems to work

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,6 @@ from langchain_community.chat_models import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings
-from langchain_core.prompts import ChatPromptTemplate
-import bs4
 from langchain import hub
 
 DIR_PATH = "test"
